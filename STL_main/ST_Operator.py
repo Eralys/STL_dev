c183276d1a6f6776bcf3bd2e2f74d06612d80fb5
jmdelouis <jmdeloui@W-IFR-23-P014.ifremer.fr>
2025-11-27 10:18:39 +0100

    Merge version that works with Synthesis of 2D/Healpix kernel

--- STL_main/ST_Operator.py
+++ STL_main/ST_Operator.py
@@ -363,29 +363,29 @@ class ST_Operator:
             
             # Compute first convolution and modulus
             data_l1 = self.wavelet_op.apply(data,j3)                  #(Nb,Nc,L,N3)
-            data_l1m[j3] = data_l1.modulus_func(copy=True)                #(Nb,Nc,L,N3) 
+            data_l1m[j3] = data_l1.modulus(copy=True)                #(Nb,Nc,L,N3) 
             
             # Compute S1 and S2
-            data_st.S1[:,:,j3,:] = data_l1m[j3].mean_func()                          #(Nb,Nc,J,L)
-            data_st.S2[:,:,j3,:] = data_l1m[j3].mean_func(square=True)               #(Nb,Nc,J,L)
+            data_st.S1[:,:,j3,:] = data_l1m[j3].mean()                          #(Nb,Nc,J,L)
+            data_st.S2[:,:,j3,:] = data_l1m[j3].mean(square=True)               #(Nb,Nc,J,L)
              
             data_l1m_l2m={}
             for j2 in range(j3+1): 
                 data_l1m_l2 = self.wavelet_op.apply(data_l1m[j2],j3) # (Nb,Nc,L2,L3,N3)
                 # S3(j2,j3) = Cov(|I*psi2|*psi3, I*psi3) 
-                data_st.S3[:,:,j2,j3,:,:] = data_l1m_l2.cov_func(
+                data_st.S3[:,:,j2,j3,:,:] = data_l1m_l2.cov(
                                  data_l1[:,:,None]         #(Nb,Nc, 1,L3,N3)
                                  )        
                                  
-                data_l1m_l2m[j2] = data_l1m_l2.modulus_func(copy=True)                #(Nb,Nc,L,N3)
+                data_l1m_l2m[j2] = data_l1m_l2.modulus(copy=True)                #(Nb,Nc,L,N3)
                 
                 for j1 in range(j2+1):
                     # S4(j1,j2,j3) = Cov(|I*psi1|*psi3, |I*psi2|*psi3)
-                    tmp=data_l1m_l2m[j1].cov_func(
+                    tmp=data_l1m_l2m[j1].cov(
                             data_l1m_l2m[j2]
                             )         
                             
-                    data_st.S4[:,:,j1,j2,j3,:,:,:] = data_l1m_l2m[j1][:,:,None,:].cov_func(
+                    data_st.S4[:,:,j1,j2,j3,:,:,:] = data_l1m_l2m[j1][:,:,None,:].cov(
                             data_l1m_l2m[j2][:,:,:,None]
                             )         
                             
